0f21f146c651a4f52e0e19f58f3741f0011aafc5
Sohan Das <www.sohandas097@gmail.com>
2024-09-12 15:29:22 +0530

    Stripe issue fixed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async(req,res) =>{
 
     // const frontend_url = "http://localhost:5173";
-    const frontend_url = server.current == "Dev" ? "http://localhost:5173" : "https://tomatoadmin.vercel.app/";
+    const frontend_url = server.current == "Dev" ? "http://localhost:5173" : "https://tomatoadmin.vercel.app";
 
     try {
         //creating new order
