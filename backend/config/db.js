f43fc5b37c6e61ac2894d49831c29004f484b985
Sohan Das <www.sohandas097@gmail.com>
2025-04-18 21:26:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://sohandas097:1234567890@cluster0.yrzqmzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect('mongodb+srv://sohandas097:1234567890@cluster0.yrzqmzs.mongodb.net/food-del?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log("DB Connected"))
     .catch(error =>console.log(error) )
 }
